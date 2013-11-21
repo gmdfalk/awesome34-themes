1d61bfded6f6d0a72cd71a177cd4b5b9fada69bb
max demian <mikar gmx de>
2013-11-21 16:21:20 +0100

    fixed vinyl

--- vinyl/theme.lua
+++ vinyl/theme.lua
@@ -39,6 +39,20 @@ else
 	theme.wallpaper = wallpaper4
 end
 
+
+if awful.util.file_readable(wallpaper1) then
+	theme.wallpaper_cmd = { "awsetbg " .. wallpaper1 }
+elseif awful.util.file_readable(wallpaper2) then
+	theme.wallpaper_cmd = { "awsetbg " .. wallpaper2 }
+elseif awful.util.file_readable(wpscript) then
+	theme.wallpaper_cmd = { "sh " .. wpscript }
+elseif awful.util.file_readable(wallpaper3) then
+	theme.wallpaper_cmd = { "awsetbg " .. wallpaper3 }
+else
+	theme.wallpaper_cmd = { "awsetbg " .. wallpaper4 }
+end
+
+
 if awful.util.file_readable(config .. "/vain/init.lua") then
     theme.useless_gap_width  = "3"
 end
